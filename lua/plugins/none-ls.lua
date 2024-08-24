61362bff11219a18b1a59c84fa7cf9aaf251898c
sltv <stas.ltv@gmail.com>
2024-08-24 13:50:49 +0200

    Fix eslint_d setup

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,11 +9,11 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint_d,
+                require("none-ls.diagnostics.eslint_d"),
                 null_ls.builtins.completion.spell,
             },
         })
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
     end,
 }
