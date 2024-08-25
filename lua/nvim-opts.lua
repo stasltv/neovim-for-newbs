16a89253905da9502b17960b5537aa0788b47711
sltv <stas.ltv@gmail.com>
2024-08-25 13:49:17 +0200

    Add autosave and autoformatting

--- lua/nvim-opts.lua
+++ lua/nvim-opts.lua
@@ -6,7 +6,20 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.opt.number = true         -- Enable line numbers
+vim.opt.number = true -- Enable line numbers
 vim.opt.numberwidth = 6
 
+-- Autosave
+vim.opt.updatetime = 5000
 
+local autosave = vim.api.nvim_create_augroup("autosave", { clear = true })
+
+vim.api.nvim_create_autocmd({ "CursorHoldI", "CursorHold" }, {
+    group = autosave,
+    callback = function()
+        if vim.bo.modified then
+            vim.lsp.buf.format()
+            vim.cmd("silent! write")
+        end
+    end,
+})
