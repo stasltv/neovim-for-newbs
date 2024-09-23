b551d18d3792e1c64f1a5dea80f6cafb42340f9e
sltv <stas.ltv@gmail.com>
2024-09-23 21:56:36 +0200

    Remove hls, update ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,64 +1,59 @@
 return {
     {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         config = function()
-            require('mason').setup()
-        end
+            require("mason").setup()
+        end,
     },
     {
-        'williamboman/mason-lspconfig.nvim',
+        "williamboman/mason-lspconfig.nvim",
         config = function()
-            require('mason-lspconfig').setup({
+            require("mason-lspconfig").setup({
                 ensure_installed = {
-                    'lua_ls',
-                    'tailwindcss',
-                    'volar',
-                    'eslint',
-                    'tsserver',
-                    'hls'
-                }
+                    "lua_ls",
+                    "tailwindcss",
+                    "volar",
+                    "eslint",
+                    "ts_ls",
+                },
             })
-        end
+        end,
     },
     {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require('lspconfig')
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.tailwindcss.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
-            lspconfig.volar.setup {
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-            }
+            lspconfig.volar.setup({
+                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+            })
             lspconfig.eslint.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
-            lspconfig.hls.setup({
-                filetypes = { 'haskell', 'lhaskell', 'cabal' },
-                capabilities = capabilities
-            })
-            local mason_registry = require('mason-registry')
-            local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
-                '/node_modules/@vue/language-server'
-            lspconfig.tsserver.setup({
+            local mason_registry = require("mason-registry")
+            local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+                .. "/node_modules/@vue/language-server"
+            lspconfig.ts_ls.setup({
                 init_options = {
                     plugins = {
                         {
-                            name = '@vue/typescript-plugin',
+                            name = "@vue/typescript-plugin",
                             location = vue_language_server_path,
-                            languages = { 'vue' },
+                            languages = { "vue" },
                         },
                     },
                 },
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
             })
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
