20693303ba1eba8040cbad76e2f5bfa807703db5
sltv <stas.ltv@gmail.com>
2025-04-06 15:26:21 +0200

    Fix warnings in Treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,35 @@ return {
     config = function()
         local config = require('nvim-treesitter.configs')
         config.setup({
+            modules = {},
+            sync_install = false,
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            ignore_install = {},
+            ensure_installed = {
+                "bash",
+                "css",
+                "gitignore",
+                "haskell",
+                "html",
+                "htmldjango",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "scss",
+                "sql",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            }
         })
     end
 }
