5df275fb11e64a0ad9ea41c53d6b22928e178211
sltv <stas.ltv@gmail.com>
2025-04-06 18:40:16 +0200

    Add Typescript, Volar, ESLint

---
+++ lua/config/lsp/volar.lua
@@ -0,0 +1,12 @@
+vim.lsp.config.volar = {
+    cmd = { "vue-language-server", "--stdio" },
+    filetypes = { "vue" },
+    root_markers = { '.git', 'package.json', 'tsconfig.json' },
+    init_options = {
+        typescript = {
+            tsdk = "/Users/sltv/.nvm/versions/node/v20.17.0/lib/node_modules/typescript/lib"
+        }
+    },
+}
+
+vim.lsp.enable("volar")
